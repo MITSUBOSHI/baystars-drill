038a7c8255848c6ce8c501f84dbc04983e6c5ed9
mitsubosh <yuya.mitsuboshi@gmail.com>
2024-10-20 20:36:38 +0900

    refactor: use json data

--- src/app/uniform-number/gallery/page.tsx
+++ src/app/uniform-number/gallery/page.tsx
@@ -1,33 +1,4 @@
-type DataType = {
-  year: number;
-  player: PlayerType;
-};
-type PlayerType = {
-  name: string;
-  name_kana: string;
-  number_calc: number;
-  number_disp: string;
-};
-const data: DataType[] = [
-  {
-    year: 2024,
-    player: {
-      name: "三浦 大輔",
-      name_kana: "みうら だいすけ",
-      number_calc: 81,
-      number_disp: "81",
-    },
-  },
-  {
-    year: 2024,
-    player: {
-      name: "林 琢真",
-      name_kana: "はやし たくま",
-      number_calc: 0,
-      number_disp: "00",
-    },
-  },
-];
+import Players2024 from '@/data/2024-players.jsonl.json';
 
 export default function Index() {
   const currentYear = 2024; // TODO
@@ -35,10 +6,10 @@ export default function Index() {
     <>
       <h1>📖 選手図鑑 📖</h1>
       <h2> Year {currentYear} </h2>
-      {data.map((d) => {
+      {Players2024.map((player) => {
         return (
-          <div key={d.player.number_disp}>
-            {d.player.number_disp} | {d.player.name}({d.player.name_kana})
+          <div key={player.number_disp}>
+            {player.number_disp} | {player.name}({player.name_kana})
           </div>
         );
       })}
