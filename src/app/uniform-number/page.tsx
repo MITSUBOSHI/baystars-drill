755b9c0d3f27ce9d2d95a31597e132d509e49524
mitsubosh <yuya.mitsuboshi@gmail.com>
2024-10-20 22:38:46 +0900

    Fix link

--- src/app/uniform-number/page.tsx
+++ src/app/uniform-number/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { registeredYears } from "@/constants/player";
 
 export default function Index() {
+  const maxYear = Math.max(...registeredYears);
+
   return (
     <>
       <h1>⚾️ 背番号 ⚾️</h1>
       <div>
-        <Link href="/uniform-number/gallery">選手図鑑</Link>
+        <Link href={`/uniform-number/gallery/${maxYear}`}>選手図鑑</Link>
         <a>計算ドリル</a>
       </div>
     </>
